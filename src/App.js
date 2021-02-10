6585bdd8980656787dbef666d48558af4b5f05f4
fahim <fhariyani9272@gmail.com>
2021-02-10 10:45:51 +0530

    set up app and add restaurants Data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import restaurantsData from "./componants/data/dishes";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log("data:", restaurantsData.dishes);
+  return <div className="App">hello World</div>;
 }
 
 export default App;
